2333f74a55ffc9f85f050d2b48fa9191155327f7
Samuel Pierce (888450) <65698929+ikidre@users.noreply.github.com>
2023-10-24 17:01:54 -0700

    all the fiels

---
+++ wp-content/themes/rtpanel-child-theme-master/js/rtp-child-package-min.js
@@ -0,0 +1,24 @@
+/*! 
+ * rtPanel JavaScript Library 
+ * @package rtPanel 
+ */
+ jQuery(document).ready(function(){
+
+
+//  	var resizeNav = function() {
+    	
+//     	var wWidth = jQuery(window).width();
+//     	// console.log(wWidth);
+//     	if(wWidth<1200) {
+// 	    	jQuery("#rtp-primary-menu").width((wWidth-20)+"px");
+// 	    	// jQuery("#rtp-primary-menu").width("200px");
+// 	    } else {
+// 	    	jQuery("#rtp-primary-menu").width("1180px");
+// 	    }
+//     }
+
+//     jQuery(window).load(resizeNav);
+//     jQuery(window).bind("resize", resizeNav);
+
+
+ });
